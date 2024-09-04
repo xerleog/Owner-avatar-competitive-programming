3fcfd719aa3f5970922bd2e3140c134279253e3a
xerleog <xerleog@gmail.com>
2024-09-04 00:33:48 -0700

    rust

---
+++ solutions/leet-code/00081-search-in-rotated-sorted-array-ii/soln.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> bool {
+        if nums.contains(&target)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
